c76b7c922bf5a3d96bd08545645787f858b62199
Satyam Yadav <satyamyadav3@gmail.com>
2024-05-06 16:25:58 +0400

    rename Icon to icon

---
+++ core/components/atoms/icon/stories/Icon.stories.ts
@@ -0,0 +1,57 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Icon } from "@/index";
+
+const meta = {
+  title: "Components/Icon",
+  component: Icon,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+} satisfies Meta<typeof Icon>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    alt: 'MDS AI Icon'
+  },
+};
+
+export const Tiny: Story = {
+  args: {
+    size: 'tiny',
+    alt: 'MDS AI Icon'
+  },
+};
+
+export const Regular: Story = {
+  args: {
+    size: 'regular',
+    alt: 'MDS AI Icon'
+  },
+};
+
+export const Medium: Story = {
+  args: {
+    size: 'medium',
+    alt: 'MDS AI Icon'
+  },
+};
+
+export const Large: Story = {
+  args: {
+    size: 'large',
+    alt: 'MDS AI Icon'
+  },
+};
+
+export const CustomSize: Story = {
+  args: {
+    height: 100,
+    width: 100,
+    alt: 'MDS AI Icon'
+  },
+};
